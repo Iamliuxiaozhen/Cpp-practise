30c17ab7ce77112454153604f65e276256f19e7c
oliver <oliverxiaozhen@outlook.com>
2025-12-24 21:54:44 +0800

    提交代码「B2005 字符三角形」

---
+++ B2005 字符三角形/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string n;
+    cin>>n;
+    cout<<"  "<<n<<endl
+        <<" "<<n<<n<<n<<endl
+        <<n<<n<<n<<n<<n<<endl;
+
+    return 0;
+}
