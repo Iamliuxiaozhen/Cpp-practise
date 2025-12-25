ee0439edc09648e1a91bb41f945533d41d34e1a3
oliver <oliverxiaozhen@outlook.com>
2025-12-25 21:15:00 +0800

    提交代码「B2016 浮点数向零舍入」

---
+++ B2016 浮点数向零舍入/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    double x;
+    cin>>x;
+    cout<< fixed << setprecision(0) <<x;
+
+    return 0;
+}
+
+//待优化，测试点状态2个未通过，状态Unaccepted
