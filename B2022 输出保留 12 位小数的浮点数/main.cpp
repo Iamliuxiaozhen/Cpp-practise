e020d00d4f0ef83531f4359a3b03ae39a9b8600d
oliver <oliverxiaozhen@outlook.com>
2025-12-26 22:34:12 +0800

    提交代码「B2022 输出保留 12 位小数的浮点数」

---
+++ B2022 输出保留 12 位小数的浮点数/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    double a;
+    cin>>a;
+
+    cout << fixed << setprecision(12) << a << endl;
+
+    return 0;
+}
+
+//这个代码可以参考B2021，将单精度改为双精度。并且修改参数。
